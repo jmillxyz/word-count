1bd5e4a22da33a68b798a416a2302db25a7928aa
Jon Miller <jondelmil@gmail.com>
2016-08-23 16:06:00 -0500

    Remove unnecessary Indexer class

--- indexer.py
+++ indexer.py
@@ -1,31 +1,30 @@
 import re
 from collections import Counter
 
-class Indexer:
 
-    def tokenize(text:str) -> list:
-        '''
-        Tokenize a blob of text.
+def tokenize(text:str) -> list:
+    '''
+    Tokenize a blob of text.
 
-        Any character other than a-z, A-Z or 0-9 is assumed to be a delimiter.
-        In Python regular expressions, \W is equivalent to all characters NOT
-        in [a-zA-Z0-9_], so one must also add the underscore as an additional
-        delimiter.
-        '''
-        return re.split(pattern=r'[\W_]+', string=text)
+    Any character other than a-z, A-Z or 0-9 is assumed to be a delimiter.
+    In Python regular expressions, \W is equivalent to all characters NOT
+    in [a-zA-Z0-9_], so one must also add the underscore as an additional
+    delimiter.
+    '''
+    return re.split(pattern=r'[\W_]+', string=text)
 
-    def count_uniques(word_list:list) -> dict:
-        """
-        Track unique instances of each word from a list, ignoring case.
+def count_uniques(word_list:list) -> dict:
+    """
+    Track unique instances of each word from a list, ignoring case.
 
-        Counter is a dictionary subclass for "counting hashable objects", in
-        this case words. Since the spec doesn't explicitly say to convert the
-        words in the list to case insensitive, we shouldn't mutate them
-        permanently.
+    Counter is a dictionary subclass for "counting hashable objects", in
+    this case words. Since the spec doesn't explicitly say to convert the
+    words in the list to case insensitive, we shouldn't mutate them
+    permanently.
 
-        Counter's most_common method returns the n most common elements and
-        their counts, which is particularly useful. However, it returns a list,
-        whereas the dict looks better.
-        """
-        return dict(Counter(w.lower() for w in word_list).most_common(10))
+    Counter's most_common method returns the n most common elements and
+    their counts, which is particularly useful. However, it returns a list,
+    whereas the dict looks better.
+    """
+    return dict(Counter(w.lower() for w in word_list).most_common(10))
 
--- indexer_test.py
+++ indexer_test.py
@@ -1,40 +1,40 @@
 import pytest
 
-from indexer import Indexer
+from indexer import tokenize, count_uniques
 
 
 def test_tokenize_with_spaces():
-    assert Indexer.tokenize('Hello there friend') == ['Hello', 'there', 'friend']
+    assert tokenize('Hello there friend') == ['Hello', 'there', 'friend']
 
 def test_tokenize_with_commas():
-    assert Indexer.tokenize('my,spacebar,is,broken') == ['my', 'spacebar', 'is', 'broken']
+    assert tokenize('my,spacebar,is,broken') == ['my', 'spacebar', 'is', 'broken']
 
 def test_tokenize_with_mixed_characters():
-    assert Indexer.tokenize('This,sentence!is crazy') == ['This', 'sentence', 'is', 'crazy']
+    assert tokenize('This,sentence!is crazy') == ['This', 'sentence', 'is', 'crazy']
 
 def test_tokenize_with_multiple_delimiters():
-    assert Indexer.tokenize('So    many    spaces') == ['So', 'many', 'spaces']
+    assert tokenize('So    many    spaces') == ['So', 'many', 'spaces']
 
 def test_tokenize_with_underscores():
-    assert Indexer.tokenize('So_many__underscores') == ['So', 'many', 'underscores']
+    assert tokenize('So_many__underscores') == ['So', 'many', 'underscores']
 
 def test_tokenize_numbers():
-    assert Indexer.tokenize('123_345 90!22*66') == ['123', '345', '90', '22', '66']
+    assert tokenize('123_345 90!22*66') == ['123', '345', '90', '22', '66']
 
 def test_tokenize_mix_alphanumeric():
-    assert Indexer.tokenize('123hello l33t__w0rds') == ['123hello', 'l33t', 'w0rds']
+    assert tokenize('123hello l33t__w0rds') == ['123hello', 'l33t', 'w0rds']
 
 def test_tokenize_with_unicode_symbols():
-    assert Indexer.tokenize('Emoji🤓are👍fun') == ['Emoji', 'are', 'fun']
+    assert tokenize('Emoji🤓are👍fun') == ['Emoji', 'are', 'fun']
 
 def test_uniques_for_same_word():
-    assert Indexer.count_uniques(['hello', 'hello', 'hello']) == {'hello':3}
+    assert count_uniques(['hello', 'hello', 'hello']) == {'hello':3}
 
 def test_uniques_mixed_case():
-    assert Indexer.count_uniques(['mixed', 'MIXED', 'mixED']) == {'mixed':3}
+    assert count_uniques(['mixed', 'MIXED', 'mixED']) == {'mixed':3}
 
 def test_top_ten_items_only_are_returned():
-    assert Indexer.count_uniques(
+    assert count_uniques(
         ['one', 'one', 'two', 'two', 'three', 'three', 'four', 'four', 'five',
          'five', 'six', 'six', 'seven', 'seven', 'eight', 'eight', 'nine',
          'nine', 'ten', 'ten', 'eleven']
